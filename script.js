aa27527f5b3318f00772be8bc38dfc501a8f90ec
egponent <egponent@gmail.com>
2022-12-01 21:18:40 -0500

    almost working

--- script.js
+++ script.js
@@ -1,15 +1,149 @@
-const e1t = document.getElementById('element1type');
-const e2t = document.getElementById('element2type');
-const e3t = document.getElementById('element3type');
-const e4t = document.getElementById('element4type');
-const e5t = document.getElementById('element5type');
+let e1t = document.getElementById('element1num').value;
+let e2t = document.getElementById('element2num').value;
+let e3t = document.getElementById('element3num').value;
+let e4t = document.getElementById('element4num').value;
+let e5t = document.getElementById('element5num').value;
 
-const e1w = document.getElementById('element1weight');
-const e2w = document.getElementById('element2weight');
-const e3w = document.getElementById('element3weight');
-const e4w = document.getElementById('element4weight');
-const e5w = document.getElementById('element5weight');
+let e1p = document.getElementById('element1percent').value;
+let e2p = document.getElementById('element2percent').value;
+let e3p = document.getElementById('element3percent').value;
+let e4p = document.getElementById('element4percent').value;
+let e5p = document.getElementById('element5percent').value;
+
+const list = [
+        1.00794,
+        4.002602,
+        6.941,
+        9.012182,
+        10.811,
+        12.0107,
+        14.0067,
+        15.9994,
+        18.9984032,
+        20.1797,
+        22.98976928,
+        24.3050,
+        26.9815386,
+        28.0855,
+        30.973762,
+        32.065,
+        35.453,
+        39.948,
+        39.0983,
+        40.078,
+        44.955912,
+        47.867,
+        50.9415,
+        51.9961,
+        54.938045,
+        55.845,
+        58.933195,
+        58.6934,
+        63.546,
+        65.38,
+        69.723,
+        72.64,
+        74.92160,
+        78.96,
+        79.904,
+        83.798,
+        85.4678,
+        87.62,
+        88.90585,
+        91.224,
+        92.90638,
+        95.96,
+        98,
+        101.07,
+        102.90550,
+        106.42,
+        107.8682,
+        112.411,
+        114.818,
+        118.710,
+        121.760,
+        127.60,
+        126.90447,
+        131.293,
+        132.9054519,
+        137.327,
+        138.90547,
+        140.116,
+        140.90765,
+        144.242,
+        145,
+        150.36,
+        151.964,
+        157.25,
+        158.92535,
+        162.500,
+        164.93032,
+        167.259,
+        168.93421,
+        173.054,
+        174.9668,
+        178.49,
+        180.94788,
+        183.84,
+        186.207,
+        190.23,
+        192.217,
+        195.084,
+        196.966569,
+        200.59,
+        204.3833,
+        207.2,
+        208.98040,
+        209,
+        210,
+        222,
+        223,
+        226,
+        227,
+        232.03806,
+        231.03588,
+        238.02891,
+        237,
+        244,
+        243,
+        247,
+        247,
+        251,
+        252,
+        257,
+        258,
+        259,
+        262,
+        267,
+        268,
+        271,
+        272,
+        270,
+        276,
+        281,
+        280,
+        285,
+        284,
+        289,
+        288,
+        293,
+        294,
+        294
+];
 
 function calc() {
 
+        let vals = [
+                e1p / list[e1t-1],
+                e2p / list[e2t-1],
+                e3p / list[e3t-1],
+                e4p / list[e4t-1],
+                e5p / list[e5t-1]
+        ];
+
+        let lowest = Math.min(...vals);
+
+        vals /= lowest;
+
+        alert("Your value is: ");
 }
