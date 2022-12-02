17529b08ffe566220faf149d4e530265eaee82cc
egponent <egponent@gmail.com>
2022-12-01 19:54:56 -0500

    wip

---
+++ script.js
@@ -0,0 +1,15 @@
+const e1t = document.getElementById('element1type');
+const e2t = document.getElementById('element2type');
+const e3t = document.getElementById('element3type');
+const e4t = document.getElementById('element4type');
+const e5t = document.getElementById('element5type');
+
+const e1w = document.getElementById('element1weight');
+const e2w = document.getElementById('element2weight');
+const e3w = document.getElementById('element3weight');
+const e4w = document.getElementById('element4weight');
+const e5w = document.getElementById('element5weight');
+
+function calc() {
+
+}
